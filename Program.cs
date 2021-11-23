f7243662cb2db6e492aac7ad13174fa92fafda34
navferty <navferty@ymail.com>
2021-11-23 23:45:29 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace HrAnswer
+{
+	public static class Program
+	{
+		private const string TemplatePath = @"template\hr-answer.txt";
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Reply for hr will be placed to clipboard. Type HR name and press enter:");
+			var name = Console.ReadLine();
+
+			var file = File.ReadAllText(TemplatePath);
+			var message = string.IsNullOrEmpty(name)
+				? file.Replace(", %UserName%", string.Empty)
+				: file.Replace("%UserName%", name);
+			Clipboard.SetText(message);
+		}
+	}
+
+	public static class Clipboard
+	{
+		public static void SetText(string text)
+		{
+			var powershell = new Process
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					FileName = "powershell",
+					Arguments = $"-command \"Set-Clipboard -Value \\\"{text}\\\"\""
+				}
+			};
+			powershell.Start();
+			powershell.WaitForExit();
+		}
+
+		public static string GetText()
+		{
+			var powershell = new Process
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					RedirectStandardOutput = true,
+					FileName = "powershell",
+					Arguments = "-command \"Get-Clipboard\""
+				}
+			};
+
+			powershell.Start();
+			string text = powershell.StandardOutput.ReadToEnd();
+			powershell.StandardOutput.Close();
+			powershell.WaitForExit();
+			return text.TrimEnd();
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # HrAnswer
+
+Get personalized answer in 2 clicks for messages from HRs.
+
+<img scr='img/chat.png' width="500" />
+
+## How to install (Windows only)
+
+- Clone this repository
+- Build project locally with command `dotnet build -c Release`
+- Add path to binaris in environment PATH variable
+
+<img scr='img/path.png' width="500" />
+
+## How to use
+
+- When you receive another message from HR, copy his/her name to clipboard
+- Press Win-R (to open Run dialog)
+<img scr='img/run.png' width="500" />
+- Run HrAnswer app and paste HR's name
+- The personalized answer is placed in clipboard, and you can just paste it into messenger in order to send your answer to HR
